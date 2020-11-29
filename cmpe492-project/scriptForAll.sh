245f317946343c3267e468abf85de37c3cce5ae3
farukozderim <farukozderim@gmail.com>
2020-11-29 07:02:01 +0300

    Delete cmpe492 files

--- cmpe492-project/scriptForAll.sh
+++
@@ -1,5 +0,0 @@
-#Be careful that this will download 40 gb of dataset and probably run ~8 hours after download
-bash scriptForDownloadingDatabase.sh
-bash scriptForRunningKallisto.sh
-bash scriptForFileMoving.sh
-bash scriptForFileRenaming.sh
